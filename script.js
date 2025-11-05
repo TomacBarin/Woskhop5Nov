521d5129305b743608c09fbdeea837e91df4f2f1
Tomac Barin Jansson <tomacbarin@me.com>
2025-11-05 11:35:42 +0100

    Almost finished

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const input = document.querySelector(".input");
 const p = document.querySelector(".lo");
 
 import { convert as tomacConvert } from "./lengthmodule.js";
+import { convert as dennisConvert } from "./weightmodule.js";
 
 btn.addEventListener("click", () => {
   let miles = parseFloat(input.value);
   let result = tomacConvert(miles, 1.6);
+  p.textContent = result;
+  console.log("Knapp har klickats!");
+});
+
+// Här är Weight
+const btnWeight = document.querySelector("#weightBtn");
+const inputWeight = document.querySelector("#inputWeight");
+const paraWeight = document.querySelector(".paraWeight");
+
+btn.addEventListener("click", () => {
+  let weight = parseFloat(inputWeight.value);
+  let result = dennisConvert(miles, 1.6);
   p.textContent = result;
   console.log("Knapp har klickats!");
 });
